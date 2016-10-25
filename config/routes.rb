1fe969522096436dce1be990dbdcd5bd540befb5
viviana rishe <vivianacuri@gmail.com>
2016-10-25 12:33:39 -0700

    renders form in the edit view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'garments/new' => 'garments#new', as: :new_garment
   get 'garments/:id' => 'garments#show', as: :garment
   get 'garments/:id/edit' => 'garments#edit', as: :edit_garment
-  put 'garments/:id' => 'garments#update'
+  patch 'garments/:id' => 'garments#update'
   delete 'garments/:id' => 'garments#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
