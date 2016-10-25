f39268a773d2352f23d4cb5bdd32ce66a020292b
viviana rishe <vivianacuri@gmail.com>
2016-10-25 10:56:36 -0700

    adds restful routes for garments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'garments/' => 'garments#index'
+  post 'garments/' => 'garments#create'
+  get 'garments/new' => 'garments#new', as: :new_garment
+  get 'garments/:id' => 'garments#show', as: :garment
+  get 'garments/:id/edit' => 'garments#edit', as: :edit_garment
+  put 'garments/:id' => 'garments#update'
+  delete 'garments/:id' => 'garments#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
